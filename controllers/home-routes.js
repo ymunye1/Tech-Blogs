a27ada970c6b3eae87ae6e1c796c325a03d80058
Yasin Munye <cyasinsadiq@gmail.com>
2022-03-24 18:49:43 -0400

    added styles and link more handlebars page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -7,7 +7,34 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/register', async (req, res) => {
-    res.render('register');
+    try {
+        res.render('register');
+    } catch (error) {
+        res.status(500).json(err)
+         console.log(err)
+    }
+    
+});
+
+
+router.get('/login', async (req, res) => {
+    try {
+        res.render('login');
+    } catch (error) {
+        res.status(500).json(err)
+         console.log(err)
+    }
+    
+});
+
+router.get('/dashboard', async (req, res) => {
+    try {
+        res.render('register');
+    } catch (error) {
+        res.status(500).json(err)
+         console.log(err)
+    }
+    
 });
 
 module.exports = router;
