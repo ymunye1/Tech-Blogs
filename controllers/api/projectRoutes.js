d0d5626847f079797fb8a1e75f7465bf5ca612f3
Yasin Munye <cyasinsadiq@gmail.com>
2022-03-26 13:58:01 -0400

    fixing connection

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Project, Comment, User } = require('../../models');
+const { Project, Comment} = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
